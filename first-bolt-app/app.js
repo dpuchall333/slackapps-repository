7fdd0b03c98a74c8440d273d7cc341fa3a2ade4a
dpuchall333 <danielapuchall@gmail.com>
2021-02-23 19:31:29 -0500

    Week 1 Assignment 4

--- first-bolt-app/app.js
+++ first-bolt-app/app.js
@@ -2,8 +2,25 @@ const {App} = require ('@slack/bolt');
 
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    channelID : process.env.CHANNEL_ID
 });
+
+app.event('message.channel', async ({ event, client }) => {
+    try {
+        const result = await client.chat.postMessage({
+            channel: channelID,
+            text:"Welcome!"
+        });
+
+        console.log(result);
+
+    }
+    catch (error) {
+      console.error(error);
+    }
+  });
+
 
 (async () => {
      //start the app
